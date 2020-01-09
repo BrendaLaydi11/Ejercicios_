1c77d2cb17935c1f22d0d0c10e29476e789a906f
Brenda <brendaleydi.ga@gmail.com>
2020-01-09 16:36:32 -0600

    Actualizacion de ramas

--- cursojs/practicas.js
+++ cursojs/practicas.js
@@ -1,44 +1,44 @@
 
 
-const Despertar = 2000
+// const Despertar = 2000
 
-const agenda = document.getElementById('agenda');
+// const agenda = document.getElementById('agenda');
 
-const despertar = new Promise((resolve, reject) => {
+// const despertar = new Promise((resolve, reject) => {
 
-    setTimeout(() => {
-        resolve()
-            ;
-    }, 2000)
-});
-
-
-function deberes(tarea) {
-    return new Promise(function (resolve, reject) {
-        document.write('Bañarse')
-        setTimeout(resolve, Despertar)
-    })
-}
-function tareas(tarea) {
-    return new Promise(function (resolve, reject) {
-        document.write('Vestirse')
-        setTimeout(resolve, Despertar)
-    })
-}
-function tarea1(tarea) {
-    return new Promise(function (resolve, reject) {
-        document.write('Desayunar')
-        setTimeout(resolve, Despertar)
-    })
-}
-
-document.write('Todo bien')
-deberes().then(() => deberes())
-    .then(() => tareas())
-    .then(() => tarea1())
+//     setTimeout(() => {
+//         resolve()
+//             ;
+//     }, 2000)
+// });
 
 
+// function deberes(tarea) {
+//     return new Promise(function (resolve, reject) {
+//         document.write('Bañarse')
+//         setTimeout(resolve, Despertar)
+//     })
+// }
+// function tareas(tarea) {
+//     return new Promise(function (resolve, reject) {
+//         document.write('Vestirse')
+//         setTimeout(resolve, Despertar)
+//     })
+// }
+// function tarea1(tarea) {
+//     return new Promise(function (resolve, reject) {
+//         document.write('Desayunar')
+//         setTimeout(resolve, Despertar)
+//     })
+// }
 
-    .catch((message) => {
-        document.write(`Error, ${message}`)
-    })
+// document.write('Todo bien')
+// deberes().then(() => deberes())
+//     .then(() => tareas())
+//     .then(() => tarea1())
+
+
+
+//     .catch((message) => {
+//         document.write(`Error, ${message}`)
+//     })
