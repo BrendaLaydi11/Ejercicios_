c1b59c735b827e6e8832cce117b8fc3a7dd425f9
Brenda <brendaleydi.ga@gmail.com>
2020-02-04 16:57:09 -0600

    ejeciciso css

---
+++ .vcode/javaEscrip/App2-Generador-de-Memes/js/script.js
@@ -0,0 +1,16 @@
+const ANCHO_DISPOSITIVO = window.innerWidth;
+
+class CreadorMemes{
+    constructor(){
+        this.imagenCanvas = document.getElementById('imagenCanvas');
+        this.imagen = document.getElementById('imagen');
+        this.textoSuperior = document.getElementById('textoSuperior');
+        this.textoInferior = document.getElementById('textoInferior');
+        this.descargarMeme = document.getElementById('descargarMeme');
+
+
+        this.crearCanvas();
+        this.agregarEventListeners();
+        }
+        
+}
