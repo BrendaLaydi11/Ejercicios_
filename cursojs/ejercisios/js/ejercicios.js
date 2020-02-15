88c52984d828d230ac69feedc2224335971b7461
Brenda <brendaleydi.ga@gmail.com>
2020-02-14 21:46:43 -0600

    avance

---
+++ cursojs/ejercisios/js/ejercicios.js
@@ -0,0 +1,12 @@
+
+
+function openForm(){
+    document.getElementById("myForm").style.display ="none";
+    document.getElementById("Form").style.display="block";
+    
+}
+function closeForm(){
+    document.getElementById("myForm").style.display ="block";
+    document.getElementById("Form").style.display ="none";
+}
+ 
